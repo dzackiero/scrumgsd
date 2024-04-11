b1dc52071b799dfc2c5e6c2ec4f3dc6c2d215939
dzackiero <dzakynashshar@gmail.com>
2024-04-11 12:01:35 +0700

    bug fixing

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 @props(["width" => "max-w-3xl"])
 
 <div
-    class="rounded-3xl w-full {{ $width }} p-8 md:p-12 bg-white border border-white bg-opacity-40">
+    class="rounded-3xl w-full {{ $width }} p-4 md:p-12 bg-white border border-white bg-opacity-40">
     {{ $slot }}
 </div>
